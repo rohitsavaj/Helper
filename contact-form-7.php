1de2b2c911878e93e5aff23f93e3e201dd87d244
Rohit savaj <rohi.ptl@gmail.com>
2019-12-09 14:49:50 +0530

    Update contact-form-7.php

--- contact-form-7.php
+++ contact-form-7.php
@@ -5,10 +5,13 @@ your-phone
 [tel* your-phone minlength:10 maxlength:15 class:form-control placeholder "Phone Number *"]
 
 your-email
-[email* your-email autocomplete:off class:form-control placeholder "Email *"]
+[email* your-email x3 autocomplete:off class:form-control placeholder "Email *"]
 
 your-message
 [textarea your-message class:form-control placeholder "Brief description of your legal issue"]
+[textarea* your-message x3 class:form-control] <!-- only rows -->
+[textarea* your-message 10x class:form-control] <!-- only columns -->
+[textarea* your-message 10x3 class:form-control] <!-- both -->
 
 your-zip
 [text your-zip class:form-control placeholder "Zip Code"]
