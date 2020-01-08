1c18d8a4cf94ec5c4496bc9f5b48ce53b10bd1cb
Rohit savaj <rohi.ptl@gmail.com>
2020-01-08 12:02:25 +0530

    update

--- theme/functions-custom.php
+++ theme/functions-custom.php
@@ -339,3 +339,29 @@ function webp_is_displayable($result, $path) {
 	return $result;
 }
 add_filter('file_is_displayable_image', 'webp_is_displayable', 10, 2);
+
+/*
+|--------------------------------------------------------------------------
+| remove custom post type slug
+| https://wordpress.org/support/topic/how-to-create-a-custom-post-type-without-extra-slug/
+| replace 'events' with your post type slug
+|--------------------------------------------------------------------------
+*/
+function na_remove_slug( $post_link, $post, $leavename ) {
+	if ( 'events' != $post->post_type || 'publish' != $post->post_status ) {
+		return $post_link;
+	}
+	$post_link = str_replace( '/' . $post->post_type . '/', '/', $post_link );
+	return $post_link;
+}
+add_filter( 'post_type_link', 'na_remove_slug', 10, 3 );
+
+function na_parse_request( $query ) {
+	if ( ! $query->is_main_query() || 2 != count( $query->query ) || ! isset( $query->query['page'] ) ) {
+		return;
+	}
+	if ( ! empty( $query->query['name'] ) ) {
+		$query->set( 'post_type', array( 'post', 'events', 'page' ) );
+	}
+}
+add_action( 'pre_get_posts', 'na_parse_request' );
--- theme/register post type.php
+++ theme/register post type.php
@@ -2,38 +2,38 @@
 
 add_action( 'init', 'codex_register_type_init' );
 function codex_register_type_init() {
-	$lbl_at = array(
-		'name'               => _x( 'Attorney', 'post type general name', 'law-ohio' ),
-		'singular_name'      => _x( 'Attorney', 'post type singular name', 'law-ohio' ),
-		'menu_name'          => _x( 'Attorney', 'admin menu', 'law-ohio' ),
-		'name_admin_bar'     => _x( 'Attorney', 'add new on admin bar', 'law-ohio' ),
-		'add_paw'            => _x( 'Add New', 'Attorney', 'law-ohio' ),
-		'add_paw_item'       => __( 'Add New Attorney', 'law-ohio' ),
-		'new_item'           => __( 'New Attorney', 'law-ohio' ),
-		'edit_item'          => __( 'Edit Attorney', 'law-ohio' ),
-		'view_item'          => __( 'View Attorney', 'law-ohio' ),
-		'all_items'          => __( 'All Attorney', 'law-ohio' ),
-		'search_items'       => __( 'Search Attorney', 'law-ohio' ),
-		'parent_item_colon'  => __( 'Parent Attorney:', 'law-ohio' ),
-		'not_found'          => __( 'No Attorney found.', 'law-ohio' ),
-		'not_found_in_trash' => __( 'No Attorney found in Trash.', 'law-ohio' )
+	$lbl_se = array(
+		'name'               => _x( 'Services', 'post type general name', 'twentysixteen' ),
+		'singular_name'      => _x( 'Services', 'post type singular name', 'twentysixteen' ),
+		'menu_name'          => _x( 'Services', 'admin menu', 'twentysixteen' ),
+		'name_admin_bar'     => _x( 'Services', 'add new on admin bar', 'twentysixteen' ),
+		'add_paw'            => _x( 'Add New', 'Services', 'twentysixteen' ),
+		'add_paw_item'       => __( 'Add New Services', 'twentysixteen' ),
+		'new_item'           => __( 'New Services', 'twentysixteen' ),
+		'edit_item'          => __( 'Edit Services', 'twentysixteen' ),
+		'view_item'          => __( 'View Services', 'twentysixteen' ),
+		'all_items'          => __( 'All Services', 'twentysixteen' ),
+		'search_items'       => __( 'Search Services', 'twentysixteen' ),
+		'parent_item_colon'  => __( 'Parent Services:', 'twentysixteen' ),
+		'not_found'          => __( 'No Services found.', 'twentysixteen' ),
+		'not_found_in_trash' => __( 'No Services found in Trash.', 'twentysixteen' )
 	);
-	$args_at = array(
-		'labels'             => $lbl_at,
-		'description'        => __( 'Description.', 'law-ohio' ),
+	$args_se = array(
+		'labels'             => $lbl_se,
+		'description'        => __( 'Description.', 'twentysixteen' ),
 		'public'             => true,
 		'publicly_queryable' => true,
 		'show_ui'            => true,
 		'show_in_menu'       => true,
 		'query_var'          => true,
-		'rewrite'            => array( 'slug' => 'practicearea', 'with_front' => false ),
+		'rewrite'            => array( 'slug' => '/', 'with_front' => false ),
 		'capability_type'    => 'post',
 		'has_archive'        => false,
 		'hierarchical'       => true,
 		'menu_casition'      => null,
-		'menu_icon'          => __( 'dashicons-portfolio', 'law-ohio' ),
-		'supports'           => array( 'title', 'editor', 'revisions', 'thumbnail' ),
+		'menu_icon'          => __( 'dashicons-portfolio', 'twentysixteen' ),
+		'supports'           => array( 'title', 'editor', 'revisions', ),
 		//'supports'           => array( 'title', 'editor', 'revisions', 'page-attributes', 'thumbnail' )
 	);
-	register_post_type( 'practicearea', $args_at );
+	register_post_type( 'services', $args_se );
 }
